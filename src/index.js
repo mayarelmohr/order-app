13b5b717d44471407efd3a82b88f1a137618ac7d
mayarelmohr <mayar.elmohr@gmail.com>
2018-12-30 20:07:25 +0200

    add redux to base

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxPromiseMiddleware from 'redux-promise-middleware';
 import './index.css';
 import App from './App';
+import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialState = {};
+
+const createStoreWithMiddleware = () => {
+  return createStore(
+    reducers,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxPromiseMiddleware()))
+  );
+};
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
