45e9d73829d494cd9baf9149bbc3a0d96c14204a
mayarelmohr <mayar.elmohr@gmail.com>
2019-01-04 21:27:28 +0200

    add global style for fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Home from './pages/home';
+import GlobalStyle from './utils/global_style';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <GlobalStyle />
         <Home />
       </div>
     );
---
+++ src/utils/global_style.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Global, css } from '@emotion/core';
+
+export const GlobalStyles = () => {
+  return (
+    <Global
+      styles={css`
+        @font-face {
+          font-family: 'ProximaNova';
+          src: url('../fonts/ProximaNova-Regular.otf')});
+          src:
+            url('../fonts/ProximaNova-Regular.woff') format('woff'),
+            url('../fonts/ProximaNova-Regular.ttf')}) format('truetype'),
+            url('../fonts/ProximaNova-Regular.woff2')}) format('woff2'),
+          font-style: normal;
+          font-weight: 300;
+          font-display: fallback;
+        }
+        @font-face {
+          font-family: 'ProximaNova';
+          src: url('../fonts/ProximaNova-Semibold.otf')});
+          src:
+            url('../fonts/ProximaNova-Semibold.woff') format('woff'),
+            url('../fonts/ProximaNova-Semibold.ttf')}) format('truetype'),
+            url('../fonts/ProximaNova-Semibold.woff2')}) format('woff2'),
+          font-style: normal;
+          font-weight: 400;
+          font-display: fallback;
+        }
+
+        html {
+          box-sizing: border-box;
+        }
+        *,
+        ::before,
+        ::after {
+          box-sizing: inherit;
+        }
+        html,
+        body {
+          scroll-behavior: smooth;
+          padding: 0;
+          margin: 0;
+        }
+        body {
+          font-family: 'ProximaNova-Regular', 'ProximaNova-Semibold';
+          font-weight: 300;
+          font-size: 17px;
+          color: #6A7A90;
+        }
+
+      `}
+    />
+  );
+};
+
+export default GlobalStyles;
