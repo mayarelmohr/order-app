cc223684dfeb3dc8da1eb96a02d1f431574d9c97
mayarelmohr <mayar.elmohr@gmail.com>
2019-01-06 01:58:43 +0200

    wrap filters for categories in a separate component

--- src/components/categories_wrapper.js
+++ src/components/categories_wrapper.js
@@ -2,59 +2,14 @@
 import React from 'react';
 import { jsx, css } from '@emotion/core';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { CategoriesFilter, CategoriesListWrapper } from '.';
-import { setFilter, setRouter } from '../actions';
+import { CategoriesListWrapper, CategoriesFilterWrapper } from '.';
 
-class CategoriesWrapper extends React.Component {
-  render() {
-    const { title, routerList, filterList } = this.props.products;
-    return (
-      <div
-        css={css`
-          background-color: #faf7fa;
-        `}
-      >
-        <CategoriesFilter
-          list={routerList}
-          itemClick={this.props.setRouter}
-          checkedValue={this.props.currentRouter}
-        >
-          <p>
-            What type of <span>{title.toLowerCase()}</span> are you looking for?
-          </p>
-        </CategoriesFilter>
-        <CategoriesFilter
-          list={filterList}
-          itemClick={this.props.setFilter}
-          checkedValue={this.props.currentFilter}
-        >
-          <p>Filter by</p>
-        </CategoriesFilter>
-        <CategoriesListWrapper />
-      </div>
-    );
-  }
-}
-const mapStateToProps = state => {
-  const { products, currentRouter, currentFilter } = state;
-  return {
-    products,
-    currentRouter,
-    currentFilter,
-  };
+const CategoriesWrapper = () => {
+  return (
+    <div>
+      <CategoriesFilterWrapper />
+      <CategoriesListWrapper />
+    </div>
+  );
 };
-const mapDispatchToProps = {
-  setRouter,
-  setFilter,
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CategoriesWrapper);
-
-CategoriesWrapper.propTypes = {
-  fetchProducts: PropTypes.func,
-  products: PropTypes,
-};
+export default CategoriesWrapper;
