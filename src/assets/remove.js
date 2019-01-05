bae26e7e62ad58169bd5fa96af75fe1d91e66512
mayarelmohr <mayar.elmohr@gmail.com>
2019-01-06 01:14:40 +0200

    add remove button for cart wrapper

---
+++ src/assets/remove.js
@@ -0,0 +1,34 @@
+import React from 'react';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+
+const wrapperStyle = css`
+  position: relative;
+  width: 20px;
+  height: 20px;
+  span {
+    width: 20px;
+    height: 2px;
+    display: inline-block;
+    background-color: #d0d0d0;
+  }
+`;
+const Remove = () => {
+  return (
+    <div css={wrapperStyle}>
+      <span
+        css={css`
+          transform: rotate(45deg);
+          position: absolute;
+          top: 9px;
+        `}
+      />
+      <span
+        css={css`
+          transform: rotate(-45deg);
+        `}
+      />
+    </div>
+  );
+};
+export default Remove;
