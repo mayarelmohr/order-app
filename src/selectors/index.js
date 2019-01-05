8a635c50f5eb4861f0f3039d531cd0f135c6852b
mayarelmohr <mayar.elmohr@gmail.com>
2019-01-06 01:19:46 +0200

    moved function that computes the descriptor value to selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -7,7 +7,7 @@ const getProducts = state => state.products;
 export default createSelector(
   [getRouter, getFilter, getProducts],
   (currentRouter, currentFilter, products) => {
-    const { router, filter, cluster } = products;
+    const { router, filter, cluster, descriptor } = products;
     let filteredList = {};
     return products.items
       .filter(
@@ -17,9 +17,25 @@ export default createSelector(
       )
       .reduce((groups, item) => {
         const clusterValue = item['properties'][cluster];
+        item['computer_descriptor'] = computeDescriptorValue(
+          descriptor,
+          item['properties']
+        );
         filteredList[clusterValue] = filteredList[clusterValue] || [];
         filteredList[clusterValue].push(item);
         return filteredList;
       }, {});
   }
 );
+function computeDescriptorValue(descriptor, propObj) {
+  let computedDescriptor = descriptor;
+  const regex = /\${(.*?)\}/;
+  descriptor.split(' ').map(val => {
+    const computedValue = regex.exec(val)[1];
+    computedDescriptor = computedDescriptor.replace(
+      regex.exec(val)[0],
+      propObj[computedValue]
+    );
+  });
+  return computedDescriptor;
+}
